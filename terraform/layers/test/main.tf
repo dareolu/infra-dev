30c2c4e2f2bfae38d6b580949e5b0409cceb0ef2
data-engineer-dare <dare.david.olufunmilayo@gmail.com>
2017-03-10 11:30:13 +0000

    refactored the project

--- terraform/layers/test/main.tf
+++ terraform/layers/test/main.tf
@@ -6,12 +6,8 @@ provider "aws" {
 resource "aws_instance" "DareServer" {
   ami           = "ami-a19782c5"
   instance_type = "t2.micro"
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.DareServer.public_ip} > ip_address.txt"
-  }
+  count         = 4
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.DareServer.id}"
-}
+#How can i specify elastic IP addresses for each of the servers created
+
