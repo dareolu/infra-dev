a591529a2dffc93f6c8f54b983d58c9c958a8d99
data-engineer-dare <dare.david.olufunmilayo@gmail.com>
2017-03-20 20:35:59 +0000

    layered cloudera manager

--- terraform/layers/cloudera/main.tf
+++ terraform/layers/cloudera/main.tf
@@ -1,9 +1,17 @@
-provider "aws" {
-  profile = "${var.aws_profile}"
-  region  = "${var.region}"
-}
+module "cm-manager" {
+  source = "./modules/cm-manager"
 
-resource "aws_instance" "cm-manger" {
-  ami           = "ami-a19782c5"
-  instance_type = "${var.instance_type}"
+  #manager              = "${var.manager}"
+  #tags                 = "${var.tags}"
+  #ami_id               = "${var.ami["trusty"]}"
+  #private_subnet_id    = "${lookup(null_resource.subnet_ids.triggers, var.cloudera_availability_zone)}"
+  #environment          = "${var.environment}"
+  #layer                = "${var.layer}"
+  #vpc_id               = "${data.terraform_remote_state.core.vpc_id}"
+  #access_from          = ["${data.terraform_remote_state.core.vpc_cidr_block}", "${var.vpc_peering_cidr}"]
+  #iam_instance_profile = "${data.terraform_remote_state.core.ec2_bootstrap_profile_name}"
+  #key_name             = "${var.key_name}"
+  #ssh_access_from      = ["${data.terraform_remote_state.support.bastion_private_ip_cidr}"]
+  #availability_zone    = "${var.cloudera_availability_zone}"
+  #security_group_ids   = ["${aws_security_group.cm-default.id}"]
 }
--- terraform/layers/cloudera/modules/cm-manager/terraform.tfvars
+++ terraform/layers/cloudera/modules/cm-manager/terraform.tfvars
@@ -4,7 +4,7 @@ instance_type = "t2.micro"
 
 availability_zone = "eu-west-2a"
 
-key_name = "test_key"
+key_name = "cdh_key"
 
 volume_size = 30
 
@@ -13,3 +13,5 @@ ami_id = "ami-a19782c5"
 instance_type = "t2.micro"
 
 region = "eu-west-1"
+
+instance_count = 1
