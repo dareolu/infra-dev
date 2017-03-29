88ca97ecccced89c0c4c3269d4a164e5816fcd5b
data-engineer-dare <dare.david.olufunmilayo@gmail.com>
2017-03-29 17:06:46 +0100

    configured ICMP for bastion

--- terraform/layers/core/security-groups.tf
+++ terraform/layers/core/security-groups.tf
@@ -1,36 +1,39 @@
 # bastion sg
+
 resource "aws_security_group" "bastion" {
-  name        = "bastion"
-  description = "Allows access to bastion"
+  name        = "bastion_public"
+  description = "Allow Access to Bastion and also used by public and private instances for load balancer access"
   vpc_id      = "${aws_vpc.vpc_spot.id}"
-}
 
-# ssh
-resource "aws_security_group_rule" "ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
+  #SSH RULE
 
-# self
-resource "aws_security_group_rule" "self" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  self              = true
-  security_group_id = "${aws_security_group.bastion.id}"
-}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# outbound
-resource "aws_security_group_rule" "outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.bastion.id}"
+  #HTTP RULE
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #ICMP RULE
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #Outbound http access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          #"-1 means from any protocol"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
